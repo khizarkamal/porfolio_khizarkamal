1c19b58ad296db3495b9c4d152f5c29a5baf5a3e
khizar <khizarkamal0321@gmail.com>
2024-08-26 12:38:04 +0500

    style: Added Wavy text underline decortion for name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,13 +13,17 @@ const header = () => {
           </span>
         </h3>
         <h3>My name is</h3>
-        <h3 className="animate-in slide-in-from-left duration-1000 font-poppins font-bold leading-10 tracking-wide flex gap-4 justify-center md:justify-start ">
-          <span className="text-purple inline-block">Khizar</span>
-          <span className="text-lightBlue mr-4 inline-block">Kamal</span>
+        <h3 className="animate-in slide-in-from-left duration-1000 font-poppins font-bold leading-10 tracking-wide flex gap-4 justify-center md:justify-start">
+          <span className="text-purple inline-block underline decoration-wavy underline-offset-4 decoration-purple decoration-4">
+            Khizar
+          </span>
+          <span className="text-lightBlue mr-4 inline-block underline underline-offset-4 decoration-wavy decoration-lightBlue decoration-4">
+            Kamal
+          </span>
         </h3>
         <h3>I build things for the web and love ☕</h3>
       </div>
-      <figure className="hidden rounded-full bg-gradient-primary sm:flex sm:items-center sm:justify-center sm:min-w-56 sm:min-h-56 sm:max-w-56 sm:max-h-56  md:min-w-80 md:min-h-80 md:max-w-80 md:max-h-80 animate-in slide-in-from-right duration-1000">
+      <figure className="hidden rounded-full bg-gradient-primary sm:flex sm:items-center sm:justify-center sm:min-w-56 sm:min-h-56 sm:max-w-56 sm:max-h-56  md:min-w-80 md:min-h-80 md:max-w-80 md:max-h-80 animate-in slide-in-from-right duration-1000 ">
         <Image
           src={profileImage}
           className="w-[95%] h-[95%] object-cover grayscale rounded-full hover:grayscale-0 duration-500 cursor-pointer"
